af51bb4053a42b6213898b9a521ab6e255f48a59
Enrico Mrass <emrass@users.noreply.github.com>
2020-12-05 18:20:39 +0000

    Initialize the website with basic information

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,22 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const AboutPage = () => (
   <Layout title="About | Next.js + TypeScript Example">
     <h1>About</h1>
-    <p>This is the about page</p>
     <p>
-      <Link href="/">
-        <a>Go home</a>
+      YABT is built by Enrico Mrass as a fun project to apply modern web
+      technologies in a real-life project, and because he did not find a baby
+      tracker application that fulfilled all his requirements on data privacy,
+      functionality, and API integrations.
+    </p>
+    <p>
+      This project is open source and maintained at{" "}
+      <Link href="https://github.com/emrass/yabt">
+        <a>https://github.com/emrass/yabt</a>
       </Link>
     </p>
   </Layout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
